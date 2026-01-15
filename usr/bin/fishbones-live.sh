d6da4954cd6be1cbfac81e8d265593aa4388c24d
Will Elliott <troutcobbler@gmail.com>
2026-01-14 17:28:30 -0700

    fixed installer popup for hyprland

--- usr/bin/fishbones-live.sh
+++ usr/bin/fishbones-live.sh
@@ -10,81 +10,80 @@ sed -i '$d' /home/user/.config/quickshell/shell.qml
 
 cat << EOF >> /home/user/.config/quickshell/shell.qml
 
-        // Install
-        PopupWindow {
-            id: installPopup
-            anchor.window: bar
-            anchor.rect.x: screen.width / 2 - (width / 2)
-            anchor.rect.y: screen.height / 2 - (height / 2)
-            implicitWidth: 480
-            implicitHeight: 120
-            visible: true
-            color: colorBg
+    // Install
+    PopupWindow {
+        id: installPopup
+        anchor.window: bar
+        anchor.rect.x: screen.width / 2 - (width / 2)
+        anchor.rect.y: screen.height / 2 - (height / 2)
+        implicitWidth: 480
+        implicitHeight: 120
+        visible: true
+        color: colorBg
+
+        Text {
+            anchors.top: parent.top
+            anchors.horizontalCenter: parent.horizontalCenter
+            anchors.topMargin: 25
+            color: colorFg
+            font {
+                family: fontFamily
+                pixelSize: fontSize
+            }
+            text: "Install now or explore the live-session?"
+        }
+
+        Rectangle {
+            anchors.bottom: parent.bottom
+            anchors.left: parent.left
+            anchors.bottomMargin: 15
+            anchors.leftMargin: 35
+            width: 190
+            height: 35
+            radius: 5
+            color: color0
 
             Text {
-                anchors.top: parent.top
+                anchors.verticalCenter: parent.verticalCenter
                 anchors.horizontalCenter: parent.horizontalCenter
-                anchors.topMargin: 25
                 color: colorFg
                 font {
                     family: fontFamily
                     pixelSize: fontSize
                 }
-                text: "Install now or explore the live-session?"
+                text: "LIVE"
             }
-
-            Rectangle {
-                anchors.bottom: parent.bottom
-                anchors.left: parent.left
-                anchors.bottomMargin: 15
-                anchors.leftMargin: 35
-                width: 190
-                height: 35
-                radius: 5
-                color: color0
-
-                Text {
-                    anchors.verticalCenter: parent.verticalCenter
-                    anchors.horizontalCenter: parent.horizontalCenter
-                    color: colorFg
-                    font {
-                        family: fontFamily
-                        pixelSize: fontSize
-                    }
-                    text: "LIVE"
-                }
-                MouseArea {
-                    anchors.fill: parent
-                    onClicked: installPopup.visible = false
-                }
+            MouseArea {
+                anchors.fill: parent
+                onClicked: installPopup.visible = false
             }
+        }
 
-            Rectangle {
-                anchors.bottom: parent.bottom
-                anchors.right: parent.right
-                anchors.bottomMargin: 15
-                anchors.rightMargin: 35
-                width: 190
-                height: 35
-                radius: 5
-                color: color0
+        Rectangle {
+            anchors.bottom: parent.bottom
+            anchors.right: parent.right
+            anchors.bottomMargin: 15
+            anchors.rightMargin: 35
+            width: 190
+            height: 35
+            radius: 5
+            color: color0
 
-                Text {
-                    anchors.verticalCenter: parent.verticalCenter
-                    anchors.horizontalCenter: parent.horizontalCenter
-                    color: colorFg
-                    font {
-                        family: fontFamily
-                        pixelSize: fontSize
-                    }
-                    text: "INSTALL"
+            Text {
+                anchors.verticalCenter: parent.verticalCenter
+                anchors.horizontalCenter: parent.horizontalCenter
+                color: colorFg
+                font {
+                    family: fontFamily
+                    pixelSize: fontSize
                 }
-                MouseArea {
-                    anchors.fill: parent
-                    onClicked: {
-                        installPopup.visible = false;
-                        Quickshell.execDetached(["sudo", "calamares"]);
-                    }
+                text: "INSTALL"
+            }
+            MouseArea {
+                anchors.fill: parent
+                onClicked: {
+                    installPopup.visible = false;
+                    Quickshell.execDetached(["sudo", "-EH", "calamares"]);
                 }
             }
         }
